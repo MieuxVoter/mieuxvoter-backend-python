6e5adb7b96db16a12c7d2ae142af4d522b81b392
domi41 <dominique.merle.41@gmail.com>
2020-04-25 05:56:07 +0200

    chore(consistency)!: use the list syntax with `DJANGO_ALLOWED_HOSTS`
    BREAKING CHANGE: old syntax won't work anymore

--- mvapi/settings.py
+++ mvapi/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_URL = os.environ['SITE_URL']
 DEBUG = os.environ['DJANGO_DEBUG'] == 'on'
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
 MAX_NUM_GRADES = int(os.environ['MAX_NUM_GRADES'])
 LANGUAGE_AVAILABLE = os.environ['LANGUAGE_AVAILABLE']
 
